7fd11c4be1b544791ff35a25f08f9881734ab41e
gen856ka-ops <gen856ka@gmail.com>
2026-06-12 16:46:10 +0900

    kuzumochi

--- index.js
+++ index.js
@@ -68,7 +68,11 @@ function onTaskTitleClicked(task) {
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     const currentValue = task.isDone;
-    task.isDone = currentValue;
+    if(currentValue==true)
+    task.isDone = false;
+    else
+    task.isDone = true;
+    
 
     return task;
 }
